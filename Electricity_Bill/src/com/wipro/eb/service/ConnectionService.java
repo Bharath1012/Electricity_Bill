3804bdc128c5d78911116c7b8078a43b8134777d
personal <personal@DESKTOP-A77IUF2>
2020-07-23 14:53:22 +0530

    Electricity_Amount

--- Electricity_Bill/src/com/wipro/eb/service/ConnectionService.java
+++ Electricity_Bill/src/com/wipro/eb/service/ConnectionService.java
@@ -9,7 +9,8 @@ public class ConnectionService
 	{
 		try
 		{
-			if(currentReading<previousReading||currentReading<0||previousReading<0)
+			int k=currentReading-previousReading;
+			if(k<0||currentReading<0||previousReading<0)
 				throw new InvalidReadingException("give correct Reading");
 			if(!(type.equalsIgnoreCase("Domestic")||type.equalsIgnoreCase("Commercial")))
 				throw new InvalidConnectionException("Invalid connection");
@@ -39,19 +40,23 @@ public class ConnectionService
 		 try
 		 {
 	            boolean val = validate(currentReading, previousReading, type);
+	            if(val==false)
+	            	return -1;
 	            if(type.equalsIgnoreCase("Domestic"))
 	            {
 	            	float[] slabs = {2.3f ,4.2f ,5.5f};
 	            	Domestic d = new Domestic(currentReading,previousReading, slabs);
 	                return d.computeBill();
 	            }
-	            else
+	            else if(type.equalsIgnoreCase("Commercial"))
 	            {	                   
 	                    float[] slabs = {5.2f, 6.8f, 8.3f};
 	                    Commercial c = new Commercial(currentReading,previousReading, slabs);      
 	                    return c.computeBill();
-	                }
-	            } 
+	            }
+	            else
+	            	return -2;
+	       } 
 		 		catch(InvalidReadingException e)
 		 		{
 	                return -1;
